d08e221d8510dcae4c72a325e58a833e299d8f22
alien054 <mehedi.alien.1@gmail.com>
2020-11-13 17:49:59 +0600

    TESTING DONEgit add .!

---
+++ src/nachos/proj2/script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+cd /home/alien/Alien/cse314/nachos_offline/alien/OS_Offline_Nachos/src/nachos/test
+gmake clean
+gmake
+clear
+cd /home/alien/Alien/cse314/nachos_offline/alien/OS_Offline_Nachos/src/nachos/proj2
+nachos
